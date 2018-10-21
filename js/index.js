b54f871997a6828df171e27c9e48a5804b0b7f98
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-21 13:29:21 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -57,3 +57,13 @@ input.addEventListener('keydown', function(e) {
 
 
 //5) 
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
