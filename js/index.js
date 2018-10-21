8a066b0656c009e4c72349c8416724a0a618b941
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-21 13:16:12 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+
+
+//1)
 const main = document.getElementById('main');
  
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+
+
+//2) 
+const input = document.querySelector('input');
+
+function whichKey(e) {
+  console.log(e.which);
+}
+
+input.addEventListener('keydown', whichKey);
